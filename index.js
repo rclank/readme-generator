d2ee144a5847a544e1de59e0a5b0fb200a1078a2
rclank <53981655+rclank@users.noreply.github.com>
2021-12-14 00:44:25 -0500

    generate markdown complete?

--- index.js
+++ index.js
@@ -1,13 +1,119 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        // project title
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a project title!');
+                return false;
+            }
+        }
+    },
+    {
+        // github username
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username? (Required)',
+        validate: usernameInput => {
+            if (usernameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        // email address
+        type: 'input',
+        name: 'email',
+        message: 'Please enter a good contact email. (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter a good contact email!');
+                return false;
+            }
+        }
+    },
+    {
+        // project description
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project'
+    },
+    {
+        // installation instructions
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions for your project'
+    },
+    {
+        // usage info
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information for your project'
+    },
+    {
+        // license
+        // choose from list of options
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license from the following options:',
+        choices: ['MIT', 'Apache', 'None']
+    },
+    {
+        // contributing guidelines
+        type: 'input',
+        name: 'contributing',
+        message: 'Please provide contribution guidelines for your project'
+    },
+    {
+        // tests
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide testing instructions for your project'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.log("that didn't work");
+            return;
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+// start by getting user input in cli via inquirer
+init()
+    .then(answers => console.log(answers));
+    // pass user input to generate markdown
+        // return constructed markdown as one giant string
+    // pass giant markdown string as input to write to a file
+        // use fs.writeFile() to write a file with full markdown we just generated
+        // return success message upon success
+    // catch any errors in above promise chain
+        // log errors caught
+
+    // .then(answers => writeToFile('README.md', generateMarkdown(answers)));
+
+    // https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,89 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { template } = require("lodash");
+
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return '';
+  }
+
+  return `
+  ![license badge](https://img.shields.io/badge/license-${license}-green)
+  `;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
+// wtf is the 'license link' supposed to be?
 function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  }
+
+  return `
+  ## License
+
+  See above badge for license info.
+
+
+  `;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(templateData) {
+  return `
+  # ${templateData.title}
 
-`;
+  ${renderLicenseBadge(license)}
+  ## Description
+
+  ${templateData.description}
+
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [License](#license)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+
+  ## Installation
+
+  ${templateData.installation}
+
+
+  ## Usage
+
+  ${templateData.usage}
+
+
+  ## Contributing
+
+  ${templateData.contributing}
+
+
+  ${renderLicenseSection(data.license)}
+  ## Tests
+
+  ${templateData.tests}
+
+
+  ## Questions
+
+  Find me on GitHub at username ${templateData.username}.
+  Also, email me at ${templateData.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
+
+// ![NPM](https://img.shields.io/npm/l/inquirer)
+// https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
